26057214fc7e43b3005e00448a9aabbd30e9404d
zzdzhou <zzd16000@163.com>
2018-07-17 08:23:20 +0800

    20180717

--- src/main/java/jack/timesheet/timesheet_20180712/web/controller/UserController.java
+++ src/main/java/jack/timesheet/timesheet_20180712/web/controller/UserController.java
@@ -5,8 +5,11 @@ import jack.timesheet.timesheet_20180712.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import java.util.Optional;
 
@@ -20,9 +23,15 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/login")
+    public String index() {
+        return UrlBasedViewResolver.REDIRECT_URL_PREFIX + "/login.html";
+    }
+
     @PostMapping("/signup")
-    public void signUp(User user) throws Exception {
+    public String signUp(User user) throws Exception {
         userService.createAnUser(user);
+        return "timesheet";
     }
 
     @PostMapping("/authentication")
