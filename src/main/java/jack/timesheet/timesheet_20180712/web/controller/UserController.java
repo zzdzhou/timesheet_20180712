68fe67d35115389a5daed9bd66a8f09d8aaee736
zzdzhou <zzd16000@163.com>
2018-07-19 08:02:40 +0800

    20180719

--- src/main/java/jack/timesheet/timesheet_20180712/web/controller/UserController.java
+++ src/main/java/jack/timesheet/timesheet_20180712/web/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.handler.AbstractHandlerMapping;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
