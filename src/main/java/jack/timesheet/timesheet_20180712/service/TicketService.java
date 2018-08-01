0abbf816a7fbdae077c443fcfdb47de7a82d4862
zzdzhou <zzd16000@163.com>
2018-08-01 08:04:34 +0800

    20180801

--- src/main/java/jack/timesheet/timesheet_20180712/service/TicketService.java
+++ src/main/java/jack/timesheet/timesheet_20180712/service/TicketService.java
@@ -2,8 +2,18 @@ package jack.timesheet.timesheet_20180712.service;
 
 import jack.timesheet.timesheet_20180712.dao.TicketRepo;
 import jack.timesheet.timesheet_20180712.entities.Ticket;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
 
 @Service
 public class TicketService {
@@ -17,5 +27,25 @@ public class TicketService {
 
     public void addATicket(Ticket ticket) {
         ticketRepo.save(ticket);
+    }
+
+
+    public void exportXlsxFile(List<Ticket> tickets, String filename) throws IOException {
+        XSSFWorkbook wb = new XSSFWorkbook();
+        try(OutputStream os = new FileOutputStream(filename)) {
+            XSSFSheet sheet1 = wb.createSheet("sheet1");
+            // todo
+            Row row = null;
+            Cell cell = null;
+            for (int r = 0; r < tickets.size(); r++) {
+                row = sheet1.createRow(r);
+                if (r == 0) {
+                    for (int c = 0; c < row.)
+                    cell = row.createCell()
+                }
+            }
+            wb.write(os);
+        }
+
     }
 }
